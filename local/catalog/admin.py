7ec0db43959a243860d9ac08ef7acbf60bc4d965
Wiwatsapon Lertworasirikul <hamlert33@gmail.com>
2023-03-06 15:39:52 +0700

    #

--- local/catalog/admin.py
+++ local/catalog/admin.py
@@ -7,13 +7,13 @@ class BooksInstanceInline(admin.TabularInline):
     
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'display_genre')
+    list_display = ('title', 'author', 'display_genre', 'display_language')
 
     inlines = [BooksInstanceInline]
 
 @admin.register(Author)
 class AuthorAdmin(admin.ModelAdmin):
-    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    list_display = ('first_name', 'last_name',  'date_of_birth', 'date_of_death')
     
     fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
 
