62aecde1ae6b61f0abadc9bd9e199601a599a842
Wiwatsapon Lertworasirikul <hamlert33@gmail.com>
2023-03-06 13:26:37 +0700

    Create migrate

--- local/local/urls.py
+++ local/local/urls.py
@@ -14,14 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-]
-# Use include() to add paths from the catalog application
-from django.urls import include
-
-urlpatterns += [
-    path('catalog/', include('catalog.urls')),
+    path('admin', admin.site.urls),
+    path('', include('catalog.urls'))
 ]
