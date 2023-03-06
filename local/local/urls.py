f29434e52cd80d68d1bcf8491360a89a55cd3d4f
Wiwatsapon Lertworasirikul <hamlert33@gmail.com>
2023-03-06 15:11:25 +0700

    #

--- local/local/urls.py
+++ local/local/urls.py
@@ -1,4 +1,4 @@
-"""local URL Configuration
+'''local URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
@@ -12,11 +12,15 @@ Class-based views
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+'''
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.views.generic import RedirectView
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin', admin.site.urls),
-    path('', include('catalog.urls'))
-]
+    path('admin/', admin.site.urls),
+    path('catalog/', include('catalog.urls')),
+    path('', RedirectView.as_view(url='catalog/')),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
